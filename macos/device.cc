10da253ed03aba3f3b7411ebcfe0b15e881afa18
Brandon Fosdick <bfoz@bfoz.net>
2010-01-25 13:35:07 +0100

    Initial implementation of class device_type for Mac OS X

---
+++ macos/device.cc
@@ -0,0 +1,121 @@
+#include <cstdarg>
+#include <cstdio>
+
+#include "device.h"
+
+void HID::macos::device_type::close()
+{
+    if( _bufferFeature )
+    {
+	delete[] _bufferFeature;
+	_bufferFeature = NULL;
+    }
+    if( _bufferInput )
+    {
+	delete[] _bufferInput;
+	_bufferInput = NULL;
+    }
+    if( _bufferOutput )
+    {
+	delete[] _bufferOutput;
+	_bufferOutput = NULL;
+    }
+    IOHIDDeviceClose(handle, kIOHIDOptionsTypeNone);
+}
+
+bool HID::macos::device_type::open(OpenMode)
+{
+    if( _bufferFeature || _bufferInput || _bufferOutput )   // Already open?
+	return true;
+
+    if( IOHIDDeviceOpen(handle, kIOHIDOptionsTypeNone) == kIOReturnSuccess )
+    {
+	int32_t value;
+	CFTypeRef property;
+
+	_lengthFeatureBuffer = 0;
+	_lengthInputBuffer = 0;
+	_lengthOutputBuffer = 0;
+
+	if((property = IOHIDDeviceGetProperty(handle, CFSTR(kIOHIDMaxFeatureReportSizeKey))) &&
+	   (CFNumberGetTypeID() == CFGetTypeID(property)) &&
+	    CFNumberGetValue((CFNumberRef)property, kCFNumberSInt32Type, &value) )
+	{
+	    _lengthFeatureBuffer = value;
+	}
+	if((property = IOHIDDeviceGetProperty(handle, CFSTR(kIOHIDMaxInputReportSizeKey))) &&
+	   (CFNumberGetTypeID() == CFGetTypeID(property)) &&
+	    CFNumberGetValue((CFNumberRef)property, kCFNumberSInt32Type, &value) )
+	{
+	    _lengthInputBuffer = value;
+	}
+	if((property = IOHIDDeviceGetProperty(handle, CFSTR(kIOHIDMaxOutputReportSizeKey))) &&
+	   (CFNumberGetTypeID() == CFGetTypeID(property)) &&
+	    CFNumberGetValue((CFNumberRef)property, kCFNumberSInt32Type, &value) )
+	{
+	    _lengthOutputBuffer = value;
+	}
+
+	// Allocate report buffers
+	if( _lengthFeatureBuffer )
+	    _bufferFeature = new uint8_t[_lengthFeatureBuffer];
+	if( _lengthInputBuffer )
+	    _bufferInput = new uint8_t[_lengthInputBuffer];
+	if( _lengthOutputBuffer )
+	    _bufferOutput = new uint8_t[_lengthOutputBuffer];
+
+	// Success?
+	if( _bufferFeature || _bufferInput || _bufferOutput )
+	    return true;
+
+	// Failure - a HID with no reports is no HID at all
+	close();
+    }
+    return false;
+}
+
+bool HID::macos::device_type::read(buffer_type&)
+{
+    return false;
+}
+
+bool HID::macos::device_type::write(const buffer_type&)
+{
+    return false;
+}
+
+// *** Reports ***
+
+bool HID::macos::device_type::feature(unsigned reportID, buffer_type& report)
+{
+    if( !_bufferFeature || (report.size() > _lengthFeatureBuffer) )
+	return false;
+    std::copy(report.begin(), report.end(), _bufferFeature);
+    return IOHIDDeviceSetReport(handle, kIOHIDReportTypeFeature, reportID, _bufferFeature, _lengthFeatureBuffer) == kIOReturnSuccess;
+}
+
+HID::buffer_type HID::macos::device_type::feature(unsigned reportID)
+{
+    CFIndex length = _lengthFeatureBuffer;
+
+    if( _bufferFeature && (IOHIDDeviceGetReport(handle, kIOHIDReportTypeFeature, reportID, _bufferFeature, &length) == kIOReturnSuccess) )
+	return buffer_type(_bufferFeature+1, _bufferFeature+length);
+    return buffer_type();
+}
+
+HID::buffer_type HID::macos::device_type::input(unsigned reportID)
+{
+    CFIndex length = _lengthInputBuffer;
+
+    if( _bufferInput && (IOHIDDeviceGetReport(handle, kIOHIDReportTypeInput, reportID, _bufferInput, &length) == kIOReturnSuccess) )
+	return buffer_type(_bufferInput+1, _bufferInput+length);
+    return buffer_type();
+}
+
+bool HID::macos::device_type::output(unsigned reportID, buffer_type& report)
+{
+    if( !_bufferOutput || (report.size() > _lengthOutputBuffer) )
+	return false;
+    std::copy(report.begin(), report.end(), _bufferOutput);
+    return IOHIDDeviceSetReport(handle, kIOHIDReportTypeOutput, reportID, _bufferOutput, _lengthOutputBuffer) == kIOReturnSuccess;
+}
---
+++ macos/device.h
@@ -0,0 +1,52 @@
+#ifndef	HID_MACOS_DEVICE_H
+#define	HID_MACOS_DEVICE_H
+
+#include <string>
+
+#include <device.h>
+
+#define	MAC_OS_X_VERSION_MIN_REQUIRED	MAC_OS_X_VERSION_10_5
+
+#include <IOKit/hid/IOHIDManager.h>
+#include <IOKit/hid/IOHIDLib.h>
+#include <IOKit/hid/IOHIDKeys.h>
+
+namespace HID
+{
+    namespace macos
+    {
+	class device_type;
+    }
+}
+
+class HID::macos::device_type : public HID::device_type
+{
+    IOHIDDeviceRef  handle;
+    size_t _lengthFeatureBuffer;
+    size_t _lengthInputBuffer;
+    size_t _lengthOutputBuffer;
+
+    uint8_t*	_bufferFeature;
+    uint8_t*	_bufferInput;
+    uint8_t*	_bufferOutput;    
+
+public:
+    device_type(IOHIDDeviceRef d, uint16_t pid, uint16_t vid, uint16_t vn) : HID::device_type("", pid, vid, vn), handle(d), _bufferFeature(0), _bufferInput(0), _bufferOutput(0) {}
+    virtual ~device_type()
+    {
+	close();
+    }
+
+    virtual void close();
+    virtual bool open(OpenMode);
+    virtual bool read(buffer_type&);
+    virtual bool write(const buffer_type&);
+
+    // Reports
+    virtual bool feature(unsigned reportID, buffer_type&);
+    virtual buffer_type feature(unsigned reportID);
+    virtual buffer_type input(unsigned reportID);
+    virtual bool output(unsigned reportID, buffer_type&);
+};
+
+#endif	// HID_MACOS_DEVICE_H
